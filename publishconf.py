6df7bf569f66554cd192ebc42bd42e9261e9ab64
Talha Mansoor <talha131@gmail.com>
2019-06-23 02:20:06 +0500

    chore(*): run black hook

--- publishconf.py
+++ publishconf.py
@@ -7,20 +7,21 @@ from __future__ import unicode_literals
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 # IMPORTANT. pelicanconf import should come after path.append
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://www.oncrashreboot.com'
+SITEURL = "https://www.oncrashreboot.com"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# DISQUS_SITENAME = ""
+# GOOGLE_ANALYTICS = ""
