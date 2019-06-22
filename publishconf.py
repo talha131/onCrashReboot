964112fe6f6b95c584c728526be88b16282325df
Talha Mansoor <talha131@gmail.com>
2019-06-23 02:39:57 +0500

    chore(*): unset SITEURL if netlify build is deploy preview

--- publishconf.py
+++ publishconf.py
@@ -12,8 +12,11 @@ sys.path.append(os.curdir)
 # IMPORTANT. pelicanconf import should come after path.append
 from pelicanconf import *
 
-# If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://www.oncrashreboot.com"
+if os.environ["CONTEXT"] == "production":
+    SITEURL = "https://www.oncrashreboot.com"
+else:
+    SITEURL = ""
+
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
