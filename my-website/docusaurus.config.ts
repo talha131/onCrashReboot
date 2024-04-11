5f2664c595f32efabe41f1f2d9a52bb97b601f19
Talha <talha131@oncrashreboot.com>
2024-04-11 14:10:16 +0500

    config: add support for csharp lang higlight

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -126,6 +126,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['csharp'],
     },
   } satisfies Preset.ThemeConfig,
 };
